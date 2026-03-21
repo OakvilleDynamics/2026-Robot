45f0d8e38eb8f8023d8e5b1daad75014b7ffd747
Oakville Dynamics Programmer <coders@oakvillerobotics.com>
2026-03-21 10:46:55 -0500

    Formatting

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -1,11 +1,10 @@
 package frc.robot.commands;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.Shooter;
+import org.littletonrobotics.junction.Logger;
 
 public class ShooterCommand extends Command {
   private final Shooter m_ShooterSubsystem;
