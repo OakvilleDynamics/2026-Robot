7465c062beeecd884dc0de783a7b11096cd8a681
Oakville Dynamics Programmer <coders@oakvillerobotics.com>
2026-03-21 11:00:17 -0500

    Add sim windows, remove mechanisms

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,13 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.DrivebaseConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.ClimberCommand;
-import frc.robot.commands.IndexCommand;
-import frc.robot.commands.IntakeCommand;
-import frc.robot.commands.ShooterCommand;
 import frc.robot.subsystems.Climber;
-import frc.robot.subsystems.Index;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.swerve.Drivetrain;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
@@ -36,10 +30,10 @@ import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final Shooter m_Shooter = new Shooter();
+  // private final Shooter m_Shooter = new Shooter();
   private final Climber m_Climber = new Climber();
-  private final Index m_Index = new Index();
-  private final Intake m_Intake = new Intake();
+  // private final Index m_Index = new Index();
+  // private final Intake m_Intake = new Intake();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   final CommandXboxController m_Driver_Controller =
@@ -147,7 +141,7 @@ public class RobotContainer {
     // Create the NamedCommands that will be used in PathPlanner
     NamedCommands.registerCommand("Climb", Commands.run(m_Climber::Climb, m_Climber));
     NamedCommands.registerCommand("Descend", Commands.run(m_Climber::Descend, m_Climber));
-    NamedCommands.registerCommand("Shoot", Commands.run(m_Shooter::Shoot, m_Shooter));
+    // NamedCommands.registerCommand("Shoot", Commands.run(m_Shooter::Shoot, m_Shooter));
 
     // Have the autoChooser pull in all PathPlanner autos as options
     autoChooser =
@@ -156,10 +150,10 @@ public class RobotContainer {
     // Set the default auto (do nothing)
     autoChooser.addDefaultOption("Do Nothing", Commands.none());
     // Set default commands for subsystems
-    m_Shooter.setDefaultCommand(new ShooterCommand(m_Shooter));
+    // m_Shooter.setDefaultCommand(new ShooterCommand(m_Shooter));
     m_Climber.setDefaultCommand(new ClimberCommand(m_Climber));
-    m_Index.setDefaultCommand(new IndexCommand(m_Index));
-    m_Intake.setDefaultCommand(new IntakeCommand(m_Intake));
+    // m_Index.setDefaultCommand(new IndexCommand(m_Index));
+    // m_Intake.setDefaultCommand(new IntakeCommand(m_Intake));
   }
 
   /**
