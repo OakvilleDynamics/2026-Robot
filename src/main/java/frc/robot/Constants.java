8eb91a633c5400b4daccdbc9f1ecabe839cfb9a2
Oakville Dynamics Programmer <coders@oakvillerobotics.com>
2026-03-26 15:12:05 -0500

    Update at 'Thu Mar 26 15:12:05 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -272,7 +272,7 @@ public final class Constants {
       // Designated motor speeds, use as last resort if you are not using PID control for the intake
       // hinge, or if you just want to set a default speed for the intake roller.
       public static final double ROLLER_SPEED = 0.8;
-      public static final double HINGE_SPEED = 0.8;
+      public static final double HINGE_SPEED = 0.3;
     }
 
     /**
--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -1,6 +1,5 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.OperatorConstants;
