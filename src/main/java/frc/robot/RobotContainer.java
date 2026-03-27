01f62ac098f4dda2974df69d1957b8a09f8de436
OakvilleDynamicsProgrammer <159196208+OakvilleDynamicsProgrammer@users.noreply.github.com>
2026-03-27 10:40:26 -0500

    Update controls to specify hinge pos
    Hinge is now set to run to position rather than a set output percentage.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.DrivebaseConstants;
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants.MechanismConstants.IntakeConstants.HingeConstants;
 import frc.robot.commands.ShooterCommand;
 import frc.robot.misc.Hub;
 import frc.robot.subsystems.Climber;
@@ -185,11 +186,11 @@ public class RobotContainer {
     // Triggers
     m_Driver_Controller
         .leftTrigger()
-        .onTrue(Commands.runOnce(m_Intake::IntakeUp, m_Intake))
+        .onTrue(Commands.runOnce(() -> m_Intake.setIntakeHingePosition(HingeConstants.downPos), m_Intake))
         .onFalse(Commands.runOnce(m_Intake::IntakeHingeStop, m_Intake));
     m_Driver_Controller
         .rightTrigger()
-        .onTrue(Commands.runOnce(m_Intake::IntakeDown, m_Intake))
+        .onTrue(Commands.runOnce(() -> m_Intake.setIntakeHingePosition(HingeConstants.upPos), m_Intake))
         .onFalse(Commands.runOnce(m_Intake::IntakeHingeStop, m_Intake));
 
     // POV (D-pad)
