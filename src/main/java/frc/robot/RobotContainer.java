46264dfdeea7a806c6db237327382648dc138905
Oakville Dynamics Programmer <coders@oakvillerobotics.com>
2026-03-27 15:05:35 -0500

    Update at 'Fri Mar 27 15:05:34 CDT 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -212,7 +212,7 @@ public class RobotContainer {
         .onTrue(Commands.runOnce(m_Intake::IntakeSpit, m_Intake))
         .onFalse(Commands.runOnce(m_Intake::IntakeRollerStop, m_Intake));
     m_Copilot_Controller
-        .button(3)
+        .button(9)
         .onTrue(Commands.runOnce(m_Index::IndexReverse, m_Index))
         .onFalse(Commands.runOnce(m_Index::IndexStop, m_Index));
     m_Copilot_Controller
